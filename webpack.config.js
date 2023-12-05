4ae29755e4c69cc10de40e6cee75a02f10aabc35
RemTitor <remtitor@gmail.com>
2023-12-05 16:33:14 +0300

    add docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     assetModuleFilename: "assets/[name][ext]",
   },
   module: {
